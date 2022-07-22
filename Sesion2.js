42f5721d991f3e31d09e3e5c9d329d41ef6d84b8
N4choCM <104795448+N4choCM@users.noreply.github.com>
2022-07-23 00:34:19 +0200

    Add files via upload

---
+++ Sesion2.js
@@ -0,0 +1,13 @@
+name = "Nacho";
+age = 26;
+developper = true;
+birthDate = new Date(1996, 04, 08);
+favouriteBook = {
+    title: "The Boy at the Top of the Mountain",
+    author: "John Boyne",
+    date: new Date(2010),
+    url: "www.theboyatthetopofthemountain.com"
+}
+
+lista = {name, age, developper, birthDate, favouriteBook};
+console.log(lista);
