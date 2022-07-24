030a4b50a7bbb1a7b5a0ffd2abcb749f922872e6
N4choCM <104795448+N4choCM@users.noreply.github.com>
2022-07-24 23:49:37 +0200

    Add files via upload

---
+++ Sesion5.js
@@ -0,0 +1,26 @@
+// Una variable que contenga tu altura en centímetros (entero)
+var cmHeight = 179;
+console.log(cmHeight);
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+var mHeight = 1.79;
+console.log(mHeight);
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+var kgWeight = 82.12;
+console.log(kgWeight);
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+var upHeight = Math.ceil(mHeight);
+console.log(upHeight);
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+var downWeight = Math.floor(kgWeight);
+console.log(downWeight);
+
+/* 
+Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" 
+es igual al máximo valor que se puede obtener en Javascript
+
+Lo siento, pero no sé qué se pide exactamente en esta parte del ejercicio.
+*/
