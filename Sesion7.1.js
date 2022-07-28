3783c69afe6462615639ec458c4cc4f79d5ff28d
N4choCM <104795448+N4choCM@users.noreply.github.com>
2022-07-28 17:40:39 +0200

    Add files via upload

---
+++ Sesion7.1.js
@@ -0,0 +1,6 @@
+const familyNames = new Set(["Nacho", "Juan", "Pepa"]);
+console.log(familyNames);
+familyNames.add("Nacho");
+console.log(familyNames);
+familyNames.add("Javascript");
+console.log(familyNames);
